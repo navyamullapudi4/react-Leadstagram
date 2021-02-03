d826b1ca4393146beee7b19030257c0636e69331
navyamullapudi <navyamullapudi4@gmail.com>
2021-02-03 23:58:52 +0000

    added header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class App extends Component {
   render() {
-    return <div className="App">learning react</div>;
+    return <div className="App">
+      <Header dark={true} className="Header"> Leadstagram</Header>
+    </div>;
   }
 }
 
