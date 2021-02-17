8fa981d7821803d938463a67619d52af8a0bd6c0
navyamullapudi <navyamullapudi4@gmail.com>
2021-02-17 23:17:38 +0000

    converted serach to a component

---
+++ client/src/components/Search.js
@@ -0,0 +1,27 @@
+const Search = ({ Card, Filter, handleFilterChange }) => {
+  return (
+    <div className="col-12">
+      <Card className="mb-3" Header="Filter">
+        <form action="">
+          <div className="form-group m-0">
+            <label htmlFor="enterName" className="sr-only">
+              Enter Name
+            </label>
+            <input
+              type="text"
+              name="enterName"
+              id="enterName"
+              className="form-control"
+              palceholder="Enter Name"
+              value={Filter}
+              onChange={handleFilterChange}
+            />
+          </div>
+          <form action=""> </form>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default Search;
