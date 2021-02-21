0dbe690e998fe4e7f7d05e60e4d9c25d72a777e4
navyamullapudi <navyamullapudi4@gmail.com>
2021-02-21 22:56:07 +0000

    Basic server  created

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
+const express = require("express"); //express library is loaded to the page
+//create an instance of express library
+const app = express();
+/*the above line is  same as below code in java
+import  com.me.Express;
+  class Gfg{
+    psvm(string args[]){
+      Express app =new Express();
+    }
+  }
+*/
 
+const port = 3100;
+
+// next is telling the app, listen to port and tell us wht is happening
+app.listen(port, () => {
+  console.log(`server started in port ${port}.`);
+});
