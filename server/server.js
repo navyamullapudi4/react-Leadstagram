acc94fa6b392b7273322e841146cc3775abbc685
navyamullapudi <navyamullapudi4@gmail.com>
2021-02-22 22:53:18 +0000

    created the basic GET  endpoint

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express"); //express library is loaded to the page
+const { getMaxListeners } = require("process");
 //create an instance of express library
 const app = express();
 /*the above line is  same as below code in java
@@ -11,6 +12,10 @@ import  com.me.Express;
 */
 
 const port = 3100;
+
+app.get("/", (req, res) => {
+  res.json("welcome to API server");
+});
 
 // next is telling the app, listen to port and tell us wht is happening
 app.listen(port, () => {
