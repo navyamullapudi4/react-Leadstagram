2c6b177ac82f5ac7845a10d738bc259fbc5cbf65
navyamullapudi <navyamullapudi4@gmail.com>
2021-02-24 19:42:44 +0000

    Displayed  Params,Query , Body

--- server/server.js
+++ server/server.js
@@ -19,13 +19,27 @@ app.get("/", (req, res) => {
   res.json("welcome to API server, mullapudi");
 });
 
-app.get("/:param", (req, res) => {
+/*app.get("/:param", (req, res) => {
   res.json({
     Message: `welcome to API server, ${req.params.param}!`,
     Q: req.query, //query string enabling
     Action: `I will create an account for ${req.query.fullname} with the username ${req.query.username} and password ${req.query.password}`
   });
-});
+});*/
+//body parser middle ware is given through express.json
+//express.json is the request handler in the below line
+app.post(
+  ["/", "/:param", "/:profile/:username"],
+  express.json(),
+  (req, res) => {
+    res.json({
+      Message: "I am in the post",
+      params: req.params,
+      Query: req.query,
+      Body: req.body
+    });
+  }
+);
 
 // next is telling the app, listen to port and tell us wht is happening
 app.listen(port, () => {
