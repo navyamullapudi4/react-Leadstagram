0d3428ee13a589ba8ea04638696dd468efe4a6fd
navyamullapudi <navyamullapudi4@gmail.com>
2021-02-25 14:12:21 +0000

    created multiple  routes and files
    *created  one for api and root
    *created one for  each todos and users under api

--- server/routes/api/todos.js
+++ server/routes/api/todos.js
@@ -1,12 +1,8 @@
 const express = require("express");
-
 const app = express.Router();
-const api = require("./api");
 
 app.get("/", (req, res) => {
-  res.json("welcome to API server!, but we are in root");
+  res.json("you're in /api/todos");
 });
-
-app.use("/api", api);
 
 module.exports = app;
