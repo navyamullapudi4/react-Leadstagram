5f1e46b63ca3856bfe9095224b654d5f8f82337c
navyamullapudi <navyamullapudi4@gmail.com>
2021-02-25 14:33:27 +0000

    Hitting  the GET gives the list

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,8 +1,43 @@
 const express = require("express");
 const app = express.Router();
 
+const users = {
+  Navya: {
+    Name: "Navya Mullapudi",
+    whatsApp: "08585858558",
+    Email: "laharimullapudi94@gmail.com",
+    Imgur: null,
+    Social: {
+      Facebook: "navya.lahari.9",
+      Twitter: null,
+      LinkedIn: "navya-mullapudi-901258128/",
+      Instagram: null,
+      Quora: null,
+      Youtube: null,
+      Reddit: "navyamullapudi4",
+      Website: null
+    }
+  },
+  Pradeep: {
+    Name: " Pradeep",
+    whatsApp: "08585963587",
+    Email: "laharimullapudi94@gmail.com",
+    Imgur: null,
+    Social: {
+      Facebook: "laharimullapudi",
+      Twitter: null,
+      LinkedIn: "navyamullapudi4",
+      Instagram: null,
+      Quora: null,
+      Youtube: null,
+      Reddit: "navyamullapudi4",
+      Website: null
+    }
+  }
+};
+
 app.get("/", (req, res) => {
-  res.json("you're in /api/users!");
+  res.json(users);
 });
 
 module.exports = app;
