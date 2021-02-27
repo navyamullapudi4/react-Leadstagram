81323dd1dae19291062d1a57b2d313ddc39c95f2
navyamullapudi <navyamullapudi4@gmail.com>
2021-02-27 20:45:10 +0000

    Added constants to files

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,40 +1,7 @@
 const express = require("express");
 const app = express.Router();
 
-const users = {
-  Navya: {
-    Name: "Navya Mullapudi",
-    whatsApp: "08585858558",
-    Email: "laharimullapudi94@gmail.com",
-    Imgur: null,
-    Social: {
-      Facebook: "navya.lahari.9",
-      Twitter: null,
-      LinkedIn: "navya-mullapudi-901258128/",
-      Instagram: null,
-      Quora: null,
-      Youtube: null,
-      Reddit: "navyamullapudi4",
-      Website: null
-    }
-  },
-  Pradeep: {
-    Name: " Pradeep",
-    whatsApp: "08585963587",
-    Email: "laharimullapudi94@gmail.com",
-    Imgur: null,
-    Social: {
-      Facebook: "laharimullapudi",
-      Twitter: null,
-      LinkedIn: "navyamullapudi4",
-      Instagram: null,
-      Quora: null,
-      Youtube: null,
-      Reddit: "navyamullapudi4",
-      Website: null
-    }
-  }
-};
+const users = require("../../constants/users.json");
 
 app.get("/", (req, res) => {
   res.json(users);
