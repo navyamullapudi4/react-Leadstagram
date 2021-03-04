f51119a787775b50dd8866e8b2ea57a887a3129f
navyamullapudi <navyamullapudi4@gmail.com>
2021-03-04 11:02:09 +0000

    Auth skeleton added

---
+++ server/routes/api/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express.Router();
+
+const users = [
+  {
+    Username: "Navya",
+    password: "navya@123",
+    FullName: "Navya Mullapudi"
+  }
+];
+
+//create new user
+//validate user
+//update existing user
+//delete existing user
+
+module.express = app;
